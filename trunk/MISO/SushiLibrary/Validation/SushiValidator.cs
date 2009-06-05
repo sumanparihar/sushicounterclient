4f6b49a1744e6386a3c061a18748dd0f82415e4a
tae.kim@serialssolutions.com <tae.kim@serialssolutions.com@c146eefe-3a86-11de-a81c-fde7d73ceb89>
2009-06-05 16:30:29 +0000

    added custom validation framework

---
+++ trunk/MISO/SushiLibrary/Validation/SushiValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SushiLibrary.Validation
+{
+    public abstract class SushiValidator
+    {
+        public bool IsValid
+        {
+            get { return IsValid;  }
+        }
+        public List<string> ErrorMessage
+        {
+            get { return _errorMessages; }
+        }
+
+        protected bool _isValid;
+        protected List<string> _errorMessages;
+
+        public virtual bool Validate(SushiReport report)
+        {
+            // initialize
+            _isValid = true;
+            _errorMessages = new List<string>();
+
+            // put sushi validation logic here
+
+            return _isValid;
+        }
+
+
+    }
+}
