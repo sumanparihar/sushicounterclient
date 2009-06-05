566dd4b560301c0bf792fe3deb601fd2ceacc9c0
tae.kim <tae.kim@serialssolutions.com>
2009-06-05 22:04:40 +0000

    add license

--- MISO/SushiLibrary/ReportLoader.cs
+++ MISO/SushiLibrary/ReportLoader.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+Copyright (c) 2009, Serials Solutions
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Xml;
@@ -7,13 +20,22 @@ namespace SushiLibrary
 {
     public static class ReportLoader
     {
-        private static string GetNodeValue(XmlNode attribute)
+        private static string GetValue(XmlNode node)
         {
-            if (attribute == null)
+            if (node == null)
             {
                 return null;
             }
-            return attribute.Value;
+            return node.Value;
+        }
+
+        private static string GetInnerText(XmlNode node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            return node.InnerText;
         }
 
         public static SushiReport LoadCounterReport(XmlDocument reportXml)
@@ -28,11 +50,11 @@ namespace SushiLibrary
             sushiReport.ReportType =
                 (CounterReportType)
                 Enum.Parse(typeof (CounterReportType),
-                           GetNodeValue(
+                           GetValue(
                                reportXml.SelectSingleNode("//s:ReportDefinition", xmlnsManager).Attributes["Name"]),
                            true);
 
-            sushiReport.Release = GetNodeValue(reportXml.SelectSingleNode("//s:ReportDefinition", xmlnsManager).Attributes["Release"]);
+            sushiReport.Release = GetValue(reportXml.SelectSingleNode("//s:ReportDefinition", xmlnsManager).Attributes["Release"]);
 
             XmlNodeList reports = reportXml.SelectNodes("//c:Report", xmlnsManager);
 
@@ -43,25 +65,30 @@ namespace SushiLibrary
                 foreach (XmlNode report in reports)
                 {
                     DateTime created;
-                    DateTime.TryParse(GetNodeValue(report.Attributes["Created"]), out created);
-                    
-                    var counterReport = new CounterReport
-                                            {
-                                                ID = GetNodeValue(report.Attributes["ID"]),
-                                                Name = GetNodeValue(report.Attributes["Name"]),
-                                                Title = GetNodeValue(report.Attributes["Title"]),
-                                                Version = GetNodeValue(report.Attributes["Version"]),
-                                                Created = created,
-                                                Vendor_Id= report.SelectSingleNode("c:Vendor/c:ID", xmlnsManager).InnerText,
-                                                Vendor_Name = report.SelectSingleNode("c:Vendor/c:Name", xmlnsManager).InnerText,
-                                                Vendor_ContactEmail = report.SelectSingleNode("c:Vendor/c:Contact/c:E-mail", xmlnsManager).InnerText,
-                                                Vendor_WebSiteUrl = report.SelectSingleNode("c:Vendor/c:WebSiteUrl", xmlnsManager).InnerText,
-                                                Vendor_LogoUrl = report.SelectSingleNode("c:Vendor/c:LogoUrl", xmlnsManager).InnerText,
-                                                Customer_ID = report.SelectSingleNode("c:Customer/c:ID", xmlnsManager).InnerText,
-                                                Customer_Name = report.SelectSingleNode("c:Customer/c:Name", xmlnsManager).InnerText,
-                                                Customer_Consortium_Code = report.SelectSingleNode("c:Customer/c:Consortium/c:Code", xmlnsManager).InnerText,
-                                                Customer_Consortium_WellKnownName = report.SelectSingleNode("c:Customer/c:Consortium/c:WellKnownName", xmlnsManager).InnerText
-                                            };
+                    DateTime.TryParse(GetValue(report.Attributes["Created"]), out created);
+
+                    var counterReport = new CounterReport();
+
+
+                    counterReport.ID = GetValue(report.Attributes["ID"]);
+                    counterReport.Name = GetValue(report.Attributes["Name"]);
+                    counterReport.Title = GetValue(report.Attributes["Title"]);
+                    counterReport.Version = GetValue(report.Attributes["Version"]);
+                    counterReport.Created = created;
+                    counterReport.Vendor_Id = GetInnerText(report.SelectSingleNode("c:Vendor/c:ID", xmlnsManager));
+                    counterReport.Vendor_Name = GetInnerText(report.SelectSingleNode("c:Vendor/c:Name", xmlnsManager));
+                    counterReport.Vendor_ContactEmail =
+                        GetInnerText(report.SelectSingleNode("c:Vendor/c:Contact/c:E-mail", xmlnsManager));
+                    counterReport.Vendor_WebSiteUrl =
+                        GetInnerText(report.SelectSingleNode("c:Vendor/c:WebSiteUrl", xmlnsManager));
+                    counterReport.Vendor_LogoUrl = GetInnerText(report.SelectSingleNode("c:Vendor/c:LogoUrl", xmlnsManager));
+                    counterReport.Customer_ID = GetInnerText(report.SelectSingleNode("c:Customer/c:ID", xmlnsManager));
+                    counterReport.Customer_Name = GetInnerText(report.SelectSingleNode("c:Customer/c:Name", xmlnsManager));
+                    counterReport.Customer_Consortium_Code =
+                        GetInnerText(report.SelectSingleNode("c:Customer/c:Consortium/c:Code", xmlnsManager));
+                    counterReport.Customer_Consortium_WellKnownName =
+                        GetInnerText(report.SelectSingleNode("c:Customer/c:Consortium/c:WellKnownName", xmlnsManager));
+
 
                     XmlNodeList reportItems = report.SelectNodes("c:Customer/c:ReportItems", xmlnsManager);
 
